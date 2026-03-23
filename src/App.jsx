2762b6e4ac9272e8ed590fe34f63b05007bab7ed
Krystian-De-Four <iikdefour@icloud.com>
2026-03-23 14:53:55 +0000

    started props tiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import HeroBanner from "./components/HeroBanner/HeroBanner";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
@@ -8,7 +9,19 @@ function App() {
       <Header />
       <main className="main-content">
         <HeroBanner />
-        <div className="all-sections"></div>
+        <div className="all-sections">
+
+          <section className="props-section">
+            <Card highlighted>
+              <h2>Props</h2>
+              <p>Props......................................................................</p>
+            </Card>
+            <Card>
+              <h2>Chips</h2>
+            </Card>
+          </section>
+
+        </div>
       </main>
     </>
   );
